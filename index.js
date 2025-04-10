c1d689d9984240c2af1c227546b3a715ec0c1910
Mohamed Diab <mohamedebrahimdiab@gmail.com>
2025-04-10 09:29:26 +0300

    modified front end link

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://masasya-v2.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
